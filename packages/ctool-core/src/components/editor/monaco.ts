5a128a7a4fc85d7b9ee1668d4b361b95a8dd8b5c
zw-95 <35022819+zw-95@users.noreply.github.com>
2024-11-26 03:29:15 +0000

    fix:修正monaco路径

--- packages/ctool-core/src/components/editor/monaco.ts
+++ packages/ctool-core/src/components/editor/monaco.ts
@@ -3,7 +3,7 @@ import ContextMenu from "./contextMenu";
 import lineInfo from "./lineInfo";
 import * as monaco from "monaco-editor";
 import "./style.css";
-loader.config({ paths: { vs: './public/monaco' } });
+loader.config({ paths: { vs: './monaco' } });
 const monacoInstance = () => {
     return loader.__getMonacoInstance();
 };
